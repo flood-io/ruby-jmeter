1a8007e719c1f60ee409352ea50f20a3cf830115
Tim Koopmans <tim.koops@gmail.com>
2013-08-14 09:24:38 +1000

    added example for immi

--- lib/ruby-jmeter/dsl.rb
+++ lib/ruby-jmeter/dsl.rb
@@ -388,7 +388,7 @@ module RubyJmeter
           :content_type => 'application/octet-stream'          
         }.merge(params)
         if response.code == 200
-          logger.info "Flood results at: #{JSON.parse(response)["response"]["link"]}"
+          logger.info "Flood results at: #{JSON.parse(response)["response"]["results"]["link"]}"
         else
           logger.fatal "Sorry there was an error: #{JSON.parse(response)["error_description"]}"
         end
