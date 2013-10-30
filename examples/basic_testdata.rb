8dd56e76c549ae100862a33c16859bf2a51655ad
Tim Koopmans <tim.koops@gmail.com>
2013-10-30 22:20:35 +1100

    add test_data method

---
+++ examples/basic_testdata.rb
@@ -0,0 +1,26 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'ruby-jmeter'
+
+test do
+  threads 1 do
+
+    # populate ${testdata} array with all results from shared data url
+    test_data 'http://54.252.206.143:8080/SRANDMEMBER/postcodes?type=text'
+
+    # populate named ${postcodes} array  with all results from shared data url
+    test_data url: 'http://54.252.206.143:8080/SRANDMEMBER/postcodes?type=text',
+              name: 'postcodes'
+
+    # populate named ${postcode} with random result from shared data url
+    test_data url: 'http://54.252.206.143:8080/SRANDMEMBER/postcodes?type=text',
+              name: 'postcode_random', match_num: 0
+
+
+    # populate named ${postcode} with exact match from shared data url
+    test_data url: 'http://54.252.206.143:8080/SRANDMEMBER/postcodes?type=text',
+              name: 'postcode_exact', regex: '^(\d+)', match_num: 1
+
+    debug_sampler
+    view_results
+  end
+end.run(path: '/usr/share/jmeter/bin/', gui: true)
