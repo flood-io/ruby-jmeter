36e49ec8281b5b5321f0c557afdd1071ce08571c
Tim Koopmans <tim.koops@gmail.com>
2013-11-09 15:22:09 +1100

    update example

--- examples/basic_flood.rb
+++ examples/basic_flood.rb
@@ -5,7 +5,7 @@ test do
   threads count: 20 do
     visit name: 'Home Page', url: 'http://google.com/'
   end
-end.flood('sd9XyrxN5yTcp1CsXU2f', {
+end.flood('vgzGtzM5hGv61exPSs6P', {
   name: 'Demo using flood.io',
   region: 'us-west-2',
   endpoint: 'dev.api.flood.io:3000'
