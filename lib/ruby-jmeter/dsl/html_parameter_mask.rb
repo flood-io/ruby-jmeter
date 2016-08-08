f0f43921ef130fc98e3d9528c593267e0694a309
Tim Koopmans <tim.koops@gmail.com>
2016-08-08 19:28:38 +1000

    updated dsl from idl

--- lib/ruby-jmeter/dsl/html_parameter_mask.rb
+++ lib/ruby-jmeter/dsl/html_parameter_mask.rb
@@ -13,8 +13,8 @@ module RubyJmeter
     def initialize(params={})
       testname = params.kind_of?(Array) ? 'HtmlParameterMask' : (params[:name] || 'HtmlParameterMask')
       @doc = Nokogiri::XML(<<-EOS.strip_heredoc)
-<ParamModifier guiclass="ParamModifierGui" testclass="ParamModifier" testname="#{testname}" enabled="true">
-  <elementProp name="ParamModifier.mask" elementType="ParamMask">
+<ConfigTestElement guiclass="ObsoleteGui" testclass="ConfigTestElement" testname="#{testname}" enabled="true">
+  <elementProp name="ParamModifier.mask" elementType="ConfigTestElement">
     <stringProp name="ParamModifier.field_name"/>
     <stringProp name="ParamModifier.prefix"/>
     <longProp name="ParamModifier.lower_bound">0</longProp>
@@ -22,7 +22,7 @@ module RubyJmeter
     <longProp name="ParamModifier.increment">1</longProp>
     <stringProp name="ParamModifier.suffix"/>
   </elementProp>
-</ParamModifier>)
+</ConfigTestElement>)
       EOS
       update params
       update_at_xpath params if params.is_a?(Hash) && params[:update_at_xpath]
