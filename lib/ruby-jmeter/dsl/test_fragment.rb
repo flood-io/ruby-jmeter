ca58dd41200e17f6bd9e9d6cefaf1164edf899f0
Tim Koopmans <tim.koops@gmail.com>
2016-08-08 19:36:14 +1000

    default enabled true

--- lib/ruby-jmeter/dsl/test_fragment.rb
+++ lib/ruby-jmeter/dsl/test_fragment.rb
@@ -13,7 +13,7 @@ module RubyJmeter
     def initialize(params={})
       testname = params.kind_of?(Array) ? 'TestFragment' : (params[:name] || 'TestFragment')
       @doc = Nokogiri::XML(<<-EOS.strip_heredoc)
-<TestFragmentController guiclass="TestFragmentControllerGui" testclass="TestFragmentController" testname="#{testname}" enabled="false"/>)
+<TestFragmentController guiclass="TestFragmentControllerGui" testclass="TestFragmentController" testname="#{testname}" enabled="true"/>)
       EOS
       update params
       update_at_xpath params if params.is_a?(Hash) && params[:update_at_xpath]
