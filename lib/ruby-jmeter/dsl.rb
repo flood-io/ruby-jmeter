a8fc20fa29ecb83a19adc7d148fd1781211e413a
Tim Koopmans <tim.koops@gmail.com>
2016-08-08 19:56:25 +1000

    refactor setup thread groups

--- lib/ruby-jmeter/dsl.rb
+++ lib/ruby-jmeter/dsl.rb
@@ -47,19 +47,7 @@ module RubyJmeter
 
 
 
-    def setup_thread_group(*args, &block)
-      params = args.shift || {}
-      params = { count: params }.merge(args.shift || {}) if params.class == Fixnum
-      params[:num_threads] = params[:count] || 1
-      params[:ramp_time] = params[:rampup] || (params[:num_threads]/2.0).ceil
-      params[:start_time] = params[:start_time] || Time.now.to_i * 1000
-      params[:end_time] = params[:end_time] || Time.now.to_i * 1000
-      params[:duration] ||= 60
-      params[:continue_forever] ||= false
-      params[:loops] = -1 if params[:continue_forever]
-      node = RubyJmeter::SetupThreadGroup.new(params)
-      attach_node(node, &block)
-    end
+
 
     def with_xhr
       http_header_manager name: 'X-Requested-With',
