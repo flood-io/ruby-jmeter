fca96143dda78ded3ef0afdcb71f40d79b672e83
Tim Koopmans <tim.koops@gmail.com>
2016-08-08 19:54:31 +1000

    refactor thread groups

--- lib/ruby-jmeter/dsl.rb
+++ lib/ruby-jmeter/dsl.rb
@@ -45,21 +45,7 @@ module RubyJmeter
 
     alias_method :auth, :http_authorization_manager
 
-    def thread_group(*args, &block)
-      params = args.shift || {}
-      params = { count: params }.merge(args.shift || {}) if params.class == Fixnum
-      params[:num_threads] = params[:count] || 1
-      params[:ramp_time] = params[:rampup] || (params[:num_threads]/2.0).ceil
-      params[:start_time] = params[:start_time] || Time.now.to_i * 1000
-      params[:end_time] = params[:end_time] || Time.now.to_i * 1000
-      params[:duration] ||= 60
-      params[:continue_forever] ||= false
-      params[:loops] = -1 if params[:continue_forever]
-      node = RubyJmeter::ThreadGroup.new(params)
-      attach_node(node, &block)
-    end
 
-    alias_method :threads, :thread_group
 
     def setup_thread_group(*args, &block)
       params = args.shift || {}
