bad3d132a008a0603848407340a745205b473bfe
Tim Koopmans <tim.koops@gmail.com>
2016-08-08 14:47:30 +1000

    tidy lines

--- lib/ruby-jmeter.rb
+++ lib/ruby-jmeter.rb
@@ -13,10 +13,17 @@ require 'ruby-jmeter/helpers/strip-heredoc'
 require 'ruby-jmeter/helpers/user-agents'
 
 lib = File.dirname(File.absolute_path(__FILE__))
+
 Dir.glob(lib + '/ruby-jmeter/dsl/*').each do |file|
   require file
 end
+
+Dir.glob(lib + '/ruby-jmeter/extensions/*').each do |file|
+  require file
+end
+
 Dir.glob(lib + '/ruby-jmeter/plugins/*').each do |file|
   require file
 end
+
 require 'ruby-jmeter/dsl'
