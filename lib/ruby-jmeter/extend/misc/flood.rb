cf8103a9ac7877e26ab7b5b695a8ba866b17577d
Tim Koopmans <tim.koops@gmail.com>
2016-08-09 18:57:09 +1000

    fix permalink

--- lib/ruby-jmeter/extend/misc/flood.rb
+++ lib/ruby-jmeter/extend/misc/flood.rb
@@ -36,7 +36,7 @@ module RubyJmeter
           content_type: 'application/octet-stream'
         }.merge(params)
         if response.code == 201
-          logger.info 'Flood results at: #{JSON.parse(response)["permalink"]}'
+          logger.info "Flood results at: #{JSON.parse(response)["permalink"]}"
         else
           logger.fatal "Sorry there was an error: #{JSON.parse(response)["error"]}"
         end
