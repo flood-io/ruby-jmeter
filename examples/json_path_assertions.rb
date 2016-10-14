87b3b94c3c759cc16194c367a5f64f45e7950b73
Tim Koopmans <tim.koops@gmail.com>
2016-10-14 18:44:23 +1100

    updated example

--- examples/json_path_assertions.rb
+++ examples/json_path_assertions.rb
@@ -4,7 +4,8 @@ require 'ruby-jmeter'
 test do
   threads count: 1, loop: 1 do
     visit 'https://api.github.com/orgs/flood-io/repos' do
-      assert json: '.name', value: 'ruby-jmeter'
+      assert json: '.name', value: '.*'
+      assert json: '.id', value: '\d+'
     end
   end
 end.run(path: '/usr/share/jmeter/bin/', gui: true)
