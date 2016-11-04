c296c1f5648c48210ddc5ff35e1cc869f3a35a2b
Cesar Palafox <cpaguila1@gmail.com>
2016-11-04 13:59:24 -0600

    Adds useExpires option to the http_cache_manager options

--- spec/http_cache_manager_spec.rb
+++ spec/http_cache_manager_spec.rb
@@ -14,4 +14,18 @@ describe 'http_cache_manager' do
       expect(cache_fragment.search(".//boolProp[@name='clearEachIteration']").first.text).to eq 'true'
     end
   end
+
+  describe 'the use_expires option should be respected' do
+    let(:doc) do
+      test do
+        cache use_expires: true
+      end.to_doc
+    end
+
+    let(:cache_fragment) { doc.search("//CacheManager") }
+
+    it 'should match on useExpires' do
+      expect(cache_fragment.search(".//boolProp[@name='useExpires']").first.text).to eq 'true'
+    end
+  end
 end
