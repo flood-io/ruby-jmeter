8acc5a374bcbabfaf2ad11a091aab9cdb6b633bf
Tim Koopmans <tim.koops@gmail.com>
2017-09-24 19:54:01 +0200

    add example with regex response headers

--- examples/regular_expression_extractor.rb
+++ examples/regular_expression_extractor.rb
@@ -6,6 +6,7 @@ test do
     visit name: 'Home', url: 'https://flooded.io' do
       regex pattern: "content='(.+?)' name='csrf-token'", name: 'csrf-token', match_number: 1, default: '424242'
       regex pattern: 'pattern', name: 'jmeter_variable_regex', variable: 'test'
+      regex pattern: 'pattern', name: 'jmeter_headers_regex', useHeaders: true
     end
   end
-end.run(path: '/usr/share/jmeter/bin/', gui: true)
+end.run(path: '/usr/local/share/jmeter-3.1/bin/', gui: true)
